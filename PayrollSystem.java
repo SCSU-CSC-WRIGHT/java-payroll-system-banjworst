4164cef0b818c64d6139950932bf0ca0d0be29f5
banjworst <sutherlands3@southernct.edu>
2025-02-19 18:17:33 -0500

    Payroll System Completed

--- PayrollSystem.java
+++ PayrollSystem.java
@@ -1,7 +1,9 @@
 public class PayrollSystem {
-    public static void Main (String[] args) {
+    public static void main(String[] args) {
         Employee employee1 = new Employee("John Doe", 101, "Engineering", 25.0, 40);
         Employee employee2 = new Employee("Jane Smith", 102, "Marketing", 30.0, 35);
         
-        
+        System.out.println(employee1.toString());
+        System.out.println(employee2.toString());
+    }
 }
